fd90cc4f0f9457ce669c56fdeb65b086891a5146
adeii <adeii@dr.com>
2018-07-04 10:02:16 +0200

    Update ksm.c

--- mm/ksm.c
+++ mm/ksm.c
@@ -1718,12 +1718,13 @@ int try_to_unmap_ksm(struct page *page, enum ttu_flags flags,
 	stable_node = page_stable_node(page);
 	if (!stable_node)
 		return SWAP_FAIL;
-
+/*
 	if (target_vma) {
 		unsigned long address = vma_address(page, target_vma);
 		ret = try_to_unmap_one(page, target_vma, address, flags);
 		goto out;
 	}
+*/
 again:
 	hlist_for_each_entry(rmap_item, hlist, &stable_node->hlist, hlist) {
 		struct anon_vma *anon_vma = rmap_item->anon_vma;
